609ffe73e730078ecc3cddbd2a278849225a21d3
geo7 <georgelenton@gmail.com>
2022-01-06 22:31:45 +0000

    Bump version

--- mpl_table/__init__.py
+++ mpl_table/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 
 from mpl_table.api import PlotParams, table_with_row_headers
--- tests/test_mpl_table.py
+++ tests/test_mpl_table.py
@@ -2,4 +2,4 @@ from mpl_table import __version__
 
 
 def test_version() -> None:
-    assert __version__ == "0.1.5"
+    assert __version__ == "0.1.6"
